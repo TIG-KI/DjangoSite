25f9b04a620650b107562fffe09a138eee53db79
TIG-KI <1763574701@qq.com>
2019-07-19 12:54:05 +0800

    对路由做了相应的更改

--- cmdb/urls.py
+++ cmdb/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path("",views.index,name="index"),
     path("login/", views.login, name="login"),
     path("register/", views.register, name="addUser"),
-    path("addUser/",views.addUser,name="addUser"),  
+    path("addUser/",views.addUser,name="addUser"),
+    path("disPUI/",views.UserList,name="UserList"),
+    path("findMovie/",views.findMovie,name="find movie")
 ]
